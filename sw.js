8ec7b9c14a24df9e073467e6cea77c16f462d84b
delizea <delizea@gmail.com>
2026-05-17 12:55:30 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+// sw.js
+
+const CACHE_NAME = "rebellion-2026-v2";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
